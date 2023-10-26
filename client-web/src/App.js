059a48e8b2f2f42ee6f7b6ea78ebbc1f7d488b16
Devam Doshi <devamdoshi212@gmail.com>
2023-10-26 22:43:57 +0530

    change directory

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div className="App">App</div>;
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Pages/Login";
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
+
+const App = () => {
+  <RouterProvider router={routes}></RouterProvider>;
+};
 
 export default App;
