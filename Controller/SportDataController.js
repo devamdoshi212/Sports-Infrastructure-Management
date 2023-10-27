eca608702278d55e247ca514a9927220e6c43cdd
Dhruv9544 <dhruvshah905@gmail.com>
2023-10-27 17:22:12 +0530

    get sport,get district

--- Controller/SportDataController.js
+++ Controller/SportDataController.js
@@ -1,10 +1,24 @@
 const SportModel = require("../Model/SportModel");
 
 module.exports.AddSport = async function (req, res) {
+    //console.log(req.file);
+    const BaseUrl = `http://localhost:9999/uploads/${req.file.originalname}`
+    //console.log(BaseUrl);
+    req.body.baseUrl = BaseUrl;
+
     let Sport = new SportModel(req.body);
-  
+    console.log(Sport);
     let data = await Sport.save();
   
     res.json({ data: data, msg: "Sport Added", rcode: 200 });
   };
   
+  module.exports.getSport = function (req, res) {
+    SportModel.find(req.query)
+      .then((data) => {
+        res.json({ data: data, msg: "Sport Retrived", rcode: 200 });
+      })
+      .catch((err) => {
+        res.json({ data: err.msg, msg: "smw", rcode: 200 });
+      });
+  };
