a86747e5b817f72a4205ab0438a2952c1a926f6d
Dhruv9544 <dhruvshah905@gmail.com>
2023-10-27 11:01:19 +0530

    verify decoded token

--- Controller/DecodeToken.js
+++ Controller/DecodeToken.js
@@ -1,15 +1,25 @@
 
 const jwt = require("jsonwebtoken");
+const UserModel = require("../Model/UsersModel");
 
 const { ACCESS_TOKEN_SECRET } = process.env;
 
-module.exports.decodedToken = function (req, res) {
+module.exports.decodedToken = async function (req, res) {
   const { token } = req.body;
+
 
   try {
     const decodedToken = jwt.verify(token, ACCESS_TOKEN_SECRET);
-    res.json({ decodedToken });
+
+   let User = await UserModel.findOne({ Email: decodedToken.Email });
+    
+    if (User && User.Password == decodedToken.Password )
+    res.json({ data: User, msg: "Verified", rcode: 200 });
+    else{
+      res.json({ data:"", msg: " Not Verified", rcode: -9 });
+    }
+    // res.json({ decodedToken });
   } catch (error) {
-    res.status(400).json({ error: "Invalid token" });
+    res.json({ data:error.msg, msg: "Error", rcode: 400 });
   }
 };
