193cd39ca236ec75ddd16cc1bce110e8b4ca66c9
HarshN187 <hnirmal913@gmail.com>
2023-10-28 01:35:48 +0530

    Update  SportComplex

--- Controller/SportsComplexController.js
+++ Controller/SportsComplexController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const SportsComplex = require("../Model/SportsComplexModel");
 
 module.exports.AddSportsComplex = async function (req, res) {
@@ -5,6 +6,7 @@ module.exports.AddSportsComplex = async function (req, res) {
     name: req.body.name,
     location: req.body.location,
     // district: { type: mongoose.SchemaTypes.ObjectId, ref: "districts" },
+    // sports: [],
     taluka: req.body.taluka,
     area: req.body.area,
     operationalSince: req.body.operationalSince,
@@ -26,11 +28,23 @@ module.exports.getSportsComplex = async function (req, res) {
 
 module.exports.updateSportsComplex = async function (req, res) {
   const id = req.params.id;
-  SportsComplex.findByIdAndUpdate({ _id: id }, req.body)
-    .then((data) => {
-      res.json({ data: data, msg: "Sport Retrived", rcode: 200 });
-    })
-    .catch((err) => {
-      res.json({ data: err.msg, msg: "smw", rcode: -9 });
-    });
+
+  console.log(req.files);
+  let resourcesarray = [];
+  req.files.forEach((ele) => {
+    resourcesarray.push(`http://localhost:9999/uploads/${ele.originalname}`);
+  });
+
+  console.log(resourcesarray);
+
+  let json = {
+    sport: req.body.sport, // Replace with the actual sport ID
+    images: resourcesarray,
+    rating: req.body.rating,
+    fees: req.body.fees,
+  };
+
+  let sportcomplex = await SportsComplex.findOne({ _id: id });
+  sportcomplex.sports.push(json);
+  let response = await sportcomplex.save();
 };
