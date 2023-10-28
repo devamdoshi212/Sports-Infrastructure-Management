7c4668a4e29687ec55608d3f632ce22be887bdb0
Rits0631 <94890637+Ritesh0604@users.noreply.github.com>
2023-10-29 00:42:57 +0530

    temp

--- Controller/DistrictController.js
+++ Controller/DistrictController.js
@@ -1,19 +1,19 @@
 const DistrictModel = require("../Model/DistrictsModel");
 
 module.exports.addDistrict = async function (req, res) {
-  let District = new DistrictModel(req.body);
+    let District = new DistrictModel(req.body);
 
-  let data = await District.save();
+    let data = await District.save();
 
-  res.json({ data: data, msg: "District Added", rcode: 200 });
+    res.json({ data: data, msg: "District Added", rcode: 200 });
 };
 
 module.exports.getDistrict = function (req, res) {
-  DistrictModel.find(req.query)
-    .then((data) => {
-      res.json({ data: data, msg: "District Retrived", rcode: 200 });
-    })
-    .catch((err) => {
-      res.json({ data: err.msg, msg: "smw", rcode: -9 });
-    });
+    DistrictModel.find(req.query)
+        .then((data) => {
+            res.json({ data: data, msg: "District Retrived", rcode: 200 });
+        })
+        .catch((err) => {
+            res.json({ data: err.msg, msg: "smw", rcode: -9 });
+        });
 };
--- Controller/SportDataController.js
+++ Controller/SportDataController.js
@@ -9,16 +9,16 @@ module.exports.AddSport = async function (req, res) {
     let Sport = new SportModel(req.body);
     console.log(Sport);
     let data = await Sport.save();
-  
+
     res.json({ data: data, msg: "Sport Added", rcode: 200 });
-  };
-  
-  module.exports.getSport = function (req, res) {
+};
+
+module.exports.getSport = function (req, res) {
     SportModel.find(req.query)
-      .then((data) => {
-        res.json({ data: data, msg: "Sport Retrived", rcode: 200 });
-      })
-      .catch((err) => {
-        res.json({ data: err.msg, msg: "smw", rcode: 200 });
-      });
-  };
+        .then((data) => {
+            res.json({ data: data, msg: "Sport Retrived", rcode: 200 });
+        })
+        .catch((err) => {
+            res.json({ data: err.msg, msg: "smw", rcode: 200 });
+        });
+};
