3e3337257e9dc70a9559749249047416982d49fc
Devam Doshi <devamdoshi212@gmail.com>
2023-10-29 03:19:29 +0530

    add instructor done (dropdown need to change)

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -2,6 +2,7 @@ const UserModel = require("../Model/UsersModel");
 const DistrictModel = require("../Model/DistrictsModel");
 const SportComplexModel = require("../Model/SportsComplexModel");
 const sendMail = require("../sendEmail");
+const InstructorModel = require("../Model/instructorModel");
 const jwt = require("jsonwebtoken");
 const { default: mongoose } = require("mongoose");
 require("dotenv").config();
@@ -10,10 +11,10 @@ const { ACCESS_TOKEN_SECRET } = process.env;
 module.exports.signup = async function (req, res) {
   let User = new UserModel(req.body);
 
-    if (User.Role == 4) {
-        const password = sendMail.passwordGenerate(8);
-        User.Password = password;
-        sendMail.sendEmail(User.Email, password);
+  if (User.Role == 4) {
+    const password = sendMail.passwordGenerate(8);
+    User.Password = password;
+    sendMail.sendEmail(User.Email, password);
 
     let data = await User.save();
     // let did = new mongoose.Types.ObjectId(data.DistrictId);
@@ -35,6 +36,20 @@ module.exports.signup = async function (req, res) {
     let response = await SportComplex.save();
     res.json({ data: data, msg: "User Added(role:3)", rcode: 200 });
   } else if (User.Role == 2) {
+    const password = sendMail.passwordGenerate(8);
+    User.Password = password;
+    sendMail.sendEmail(User.Email, password);
+    let data = await User.save();
+
+    let instructordata = {
+      userId: data._id,
+      createdBy: req.body.createdBy,
+      sports: req.body.sports,
+    };
+    let Instructor = new InstructorModel(instructordata);
+
+    let data2 = await Instructor.save();
+    res.json({ data: { data2, data }, msg: "User Added(role:2)", rcode: 200 });
   } else if (User.Role == 1) {
     const password = sendMail.passwordGenerate(8);
     User.Password = password;
@@ -48,30 +63,30 @@ module.exports.signup = async function (req, res) {
 };
 
 module.exports.login = async function (req, res) {
-    const { Email, Password } = req.body;
+  const { Email, Password } = req.body;
 
-    let User = await UserModel.findOne({ Email: Email });
+  let User = await UserModel.findOne({ Email: Email });
 
-    if (User && User.Password == Password) {
-        const accesstoken = jwt.sign(
-            { Email: User.Email, Password: User.Password },
-            ACCESS_TOKEN_SECRET,
-            { expiresIn: "1d" }
-        );
-        res.json({ data: User, msg: "login done", token: accesstoken, rcode: 200 });
-    } else {
-        res.json({ data: req.body, msg: "Invalid credential", rcode: -9 });
-    }
+  if (User && User.Password == Password) {
+    const accesstoken = jwt.sign(
+      { Email: User.Email, Password: User.Password },
+      ACCESS_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    res.json({ data: User, msg: "login done", token: accesstoken, rcode: 200 });
+  } else {
+    res.json({ data: req.body, msg: "Invalid credential", rcode: -9 });
+  }
 };
 
 module.exports.getUser = function (req, res) {
-    UserModel.find(req.query)
-        .then((data) => {
-            res.json({ data: data, msg: "User Retrived", rcode: 200 });
-        })
-        .catch((err) => {
-            res.json({ data: err.msg, msg: "smw", rcode: -9 });
-        });
+  UserModel.find(req.query)
+    .then((data) => {
+      res.json({ data: data, msg: "User Retrived", rcode: 200 });
+    })
+    .catch((err) => {
+      res.json({ data: err.msg, msg: "smw", rcode: -9 });
+    });
 };
 
 module.exports.getUserWithDistrict = function (req, res) {
