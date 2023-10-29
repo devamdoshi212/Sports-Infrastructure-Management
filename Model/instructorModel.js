cfefbe2faa80180847e7d9555e076163a7e53556
Uday Gohel <goheluday6445@gmail.com>
2023-10-29 12:11:03 +0530

    Instructor with data table and populate with user to get unstructor name

--- Model/instructorModel.js
+++ Model/instructorModel.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const instructorSchema = new mongoose.Schema(
-    {
-      userId: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
-      createdBy: { type: mongoose.SchemaTypes.ObjectId, ref: "managers" },
-      sports: [
-        {
-          sport: { type: mongoose.SchemaTypes.ObjectId, ref: "sports" },
-          timeSlot: [{ from: String, to: String }],
-          experience: Number, //in years
-        },
-      ],
-    },
-    { timestamps: true }
-  );
+  {
+    userId: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+    createdBy: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+    sports: [
+      {
+        sport: { type: mongoose.SchemaTypes.ObjectId, ref: "Sports" },
+        timeSlot: [{ from: String, to: String }],
+        experience: Number, //in years
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-  module.exports = mongoose.model('instructor',instructorSchema)
+module.exports = mongoose.model("instructor", instructorSchema);
