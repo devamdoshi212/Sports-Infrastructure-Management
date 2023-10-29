ea9cbca40aaa64d2878ae340d5496313be416fa2
HarshN187 <hnirmal913@gmail.com>
2023-10-29 23:24:32 +0530

    session model changes

--- Model/SessionModel.js
+++ Model/SessionModel.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 
-const sessionSchema = new mongoose.Schema({
-  date: Date,
-  sportscomplex: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "sportcomplexes",
-  },
-  enrolls: [
-    {
-      userId: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
-      entry: Date.now(),
-      exit: Date,
+const sessionSchema = new mongoose.Schema(
+  {
+    sportscomplex: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "sportcomplexes",
     },
-  ],
-});
+    enrolls: [
+      {
+        userId: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+        entry: Date,
+        exit: Date,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("sessions", sessionSchema);
