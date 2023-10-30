fa0493ed7e2cc44a4b0f4399d2fbc9248efa38e9
Devam Doshi <devamdoshi212@gmail.com>
2023-10-31 00:51:26 +0530

    Search and dropdown

--- client-app/screens/General/General.js
+++ client-app/screens/General/General.js
@@ -3,15 +3,34 @@ import Search from "./Search";
 import Login from "./Login";
 import { View } from "react-native";
 const Tab = createBottomTabNavigator();
-
-const General = () => {
+import { FontAwesome } from "@expo/vector-icons";
+const General = ({ navigation }) => {
   return (
-    <View>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Search} />
-        <Tab.Screen name="Settings" component={Login} />
-      </Tab.Navigator>
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          // Set icon names based on route names (customize as needed)
+          if (route.name === "Search") {
+            iconName = "search";
+          } else if (route.name === "Profile") {
+            iconName = "user";
+          }
+
+          // Return icon component
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "blue", // Color of the active tab icon
+        tabBarInactiveTintColor: "gray", // Color of the inactive tab icon
+        tabBarStyle: { backgroundColor: "white" }, // Background color of the tab bar
+        // showLabel: false,
+      })}
+    >
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Profile" component={Login} />
+    </Tab.Navigator>
   );
 };
 export default General;
