3fba37367dc534368df2bec20108031e0d1a8968
Devam Doshi <devamdoshi212@gmail.com>
2023-11-01 01:09:08 +0530

    QR done and Add athelte pending

--- client-app/screens/Athelte/QR.js
+++ client-app/screens/Athelte/QR.js
@@ -1,7 +1,51 @@
-import React from "react";
-import { Text } from "react-native";
-const QR = () => {
-  return <Text>QR</Text>;
-};
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useEffect } from "react";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default QR;
+export default function QR() {
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const [scanData, setScanData] = React.useState();
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <View style={styles.container}>
+        <Text>Please grant camera permissions to app.</Text>
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanData(data);
+    console.log(`Data: ${data}`);
+    console.log(`Type: ${type}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        style={StyleSheet.absoluteFillObject}
+        onBarCodeScanned={scanData ? undefined : handleBarCodeScanned}
+      />
+      {scanData && (
+        <Button title="Scan Again?" onPress={() => setScanData(undefined)} />
+      )}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
