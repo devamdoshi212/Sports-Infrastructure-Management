06facfa3c7c22ff5ad1971bc03f4575555e1dc6c
Devam Doshi <devamdoshi212@gmail.com>
2023-11-01 13:52:03 +0530

    get deta from backend

--- Controller/SupervisorController.js
+++ Controller/SupervisorController.js
@@ -8,7 +8,24 @@ async function getInstructorForPayment(req, res) {
     .populate("userId");
   let myArr = [];
 
-  res.json({ rcode: 200, instructor });
+  for (let index = 0; index < instructor.length; index++) {
+    const element = instructor[index];
+    let timeslot = "";
+    for (let i = 0; i < element.sports.length; i++) {
+      const e = element.sports[i];
+      if (e.sport.toString() == sportId) {
+        timeslot = e.timeSlot;
+      }
+    }
+    let obj = {
+      instructorname: element.userId.Name,
+      instructorid: element._id,
+      timeslot: timeslot,
+    };
+    myArr.push(obj);
+  }
+
+  res.json({ rcode: 200, data: myArr, instructor: instructor });
 }
 
 module.exports = {
