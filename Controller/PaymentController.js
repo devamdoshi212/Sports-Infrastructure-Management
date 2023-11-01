de1e5434f240a3ad47cc06a0afd11b8c1ad625a8
Devam Doshi <devamdoshi212@gmail.com>
2023-11-01 20:30:21 +0530

    Payment Done

--- Controller/PaymentController.js
+++ Controller/PaymentController.js
@@ -1,47 +1,50 @@
-const PaymentModel = require('../Model/PaymentModel')
+const PaymentModel = require("../Model/PaymentModel");
+const athleteModel = require("./../Model/athleteModel");
+module.exports.addPayment = async function (req, res) {
+  let Payment = new PaymentModel(req.body);
 
-module.exports.addPayment = async function(req,res){
+  let data = await Payment.save();
+  let athlete = await athleteModel.findOne({ _id: data.athleteId });
+  console.log(athlete);
+  athlete.createdBy = req.body.supervisorId;
+  athlete.payments.push(data._id);
+  let data1 = await athlete.save();
+  console.log(data1);
 
-    let Payment = new PaymentModel(req.body)
+  res.json({ data: data, data1: data1, msg: "Payment Generated", rcode: 200 });
+};
 
-    let data = await Payment.save();
-
-    console.log(data);
-
-    res.json({data:data , msg: "Payment Generated", rcode: 200})
-}
-
-module.exports.getAllPayments = async function(req,res){
-
-    PaymentModel.find(req.query)
+module.exports.getAllPayments = async function (req, res) {
+  PaymentModel.find(req.query)
     .then((data) => {
       res.json({ data: data, msg: "Payment Retrived", rcode: 200 });
     })
     .catch((err) => {
       res.json({ data: err.msg, msg: "smw", rcode: -9 });
     });
+};
 
-}
+module.exports.updatePayment = async function (req, res) {
+  const id = req.params.id;
+  let Payment = await PaymentModel.findOne({ _id: id });
 
-module.exports.updatePayment = async function(req,res){
+  if (req.body.duration !== undefined) {
+    Payment.duration = req.body.duration;
+  }
 
-    const id = req.params.id
-    let Payment = await PaymentModel.findOne({ _id: id });
-    
-    if (req.body.duration !== undefined) {
-        Payment.duration = req.body.duration;
-    }
+  if (req.body.timeSlot !== undefined) {
+    Payment.timeSlot = req.body.timeSlot;
+  }
 
-    if (req.body.timeSlot !== undefined) {
-        Payment.timeSlot = req.body.timeSlot;
-    }
-
-  
-    try {
-        let response = await Payment.save();
-        res.json({ data: response, msg: "payment updated successfully", rcode: 200 });
-    } catch (error) {
-        console.error(error);
-        res.json({ data:error.msg, msg: "smw", rcode: -9 });
-    }
-} 
+  try {
+    let response = await Payment.save();
+    res.json({
+      data: response,
+      msg: "payment updated successfully",
+      rcode: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({ data: error.msg, msg: "smw", rcode: -9 });
+  }
+};
