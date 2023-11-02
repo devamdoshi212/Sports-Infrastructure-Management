edd592d903bdbc0d45a29f71b82ed365cf6480f2
Uday Gohel <goheluday6445@gmail.com>
2023-11-02 17:03:41 +0530

    complaint doen

--- Controller/ComplaintController.js
+++ Controller/ComplaintController.js
@@ -1,46 +1,50 @@
-const ComplaintModel = require('../Model/ComplaintModel')
+const ComplaintModel = require("../Model/ComplaintModel");
 
 module.exports.addComplaint = async function (req, res) {
+  // const BaseUrl = `http://localhost:9999/Complains/${req.file.originalname}`
 
-   // const BaseUrl = `http://localhost:9999/Complains/${req.file.originalname}`
+  //req.body.baseUrl = BaseUrl;
 
-    //req.body.baseUrl = BaseUrl;
+  let Complaint = new ComplaintModel(req.body);
 
-    let Complaint = new ComplaintModel(req.body)
+  let data = await Complaint.save();
 
-    let data = await Complaint.save();
+  console.log(data);
 
-    console.log(data);
-
-    res.json({ data: data, msg: "Complaint Generated", rcode: 200 })
-}
+  res.json({ data: data, msg: "Complaint Generated", rcode: 200 });
+};
 
 module.exports.getAllComplaints = async function (req, res) {
-
-    ComplaintModel.find(req.query)
-        .then((data) => {
-            res.json({ data: data, msg: "Complaint Retrived", rcode: 200 });
-        })
-        .catch((err) => {
-            res.json({ data: err.msg, msg: "smw", rcode: -9 });
-        });
-
-}
+  ComplaintModel.find(req.query)
+    .populate("userId")
+    .then((data) => {
+      res.json({ data: data, msg: "Complaint Retrived", rcode: 200 });
+    })
+    .catch((err) => {
+      res.json({ data: err.msg, msg: "smw", rcode: -9 });
+    });
+};
 
 module.exports.updateComplaint = async function (req, res) {
+  const id = req.params.id;
+  let Complaint = await ComplaintModel.findOne({ _id: id });
 
-    const id = req.params.id
-    let Complaint = await ComplaintModel.findOne({ _id: id });
+  if (req.body.status !== undefined) {
+    Complaint.status = req.body.status;
+  }
 
-    if (req.body.status !== undefined) {
-        Complaint.status = req.body.status;
-    }
-
-    try {
-        let response = await Complaint.save();
-        res.json({ data: response, msg: "Complaint updated successfully", rcode: 200 });
-    } catch (error) {
-        console.error(error);
-        res.json({ data: error.msg, msg: "smw", rcode: -9 });
-    }
-} 
+  if (req.body.level !== undefined) {
+    Complaint.level = req.body.level;
+  }
+  try {
+    let response = await Complaint.save();
+    res.json({
+      data: response,
+      msg: "Complaint updated successfully",
+      rcode: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({ data: error.msg, msg: "smw", rcode: -9 });
+  }
+};
