808b1399237c90fe397edacd8f479747213d8413
Ishasanghani <102859564+Ishasanghani@users.noreply.github.com>
2023-11-04 08:30:07 +0530

    admin dashborad changes

--- client-web/src/Pages/AdminDashboard.js
+++ client-web/src/Pages/AdminDashboard.js
@@ -26,41 +26,41 @@ const AdminDashboard = () => {
     <div className="m-5">
       {/* <AllBlogs /> */}
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
-        <div className="h-32 text-center rounded-lg bg-gray-300 ">
-          <div className=" font-bold p-5 text-lg">Total Active Complaints</div>
-          <div className="p-5 ">{visible && data.activecomplaints}</div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className=" font-bold p-5 text-3xl">Total Active Complaints</div>
+          <div className="p-5 text-2xl ">{visible && data.activecomplaints}</div>
         </div>
-        <div className="h-32 text-center rounded-lg bg-gray-300 ">
-          <div className=" font-bold p-5 text-lg">Total Solved Complaints</div>
-          <div className="p-5 ">{visible && data.solvedComplaints}</div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className=" font-bold p-5 text-3xl">Total Solved Complaints</div>
+          <div className="p-5 text-2xl">{visible && data.solvedComplaints}</div>
         </div>
-        <div className="h-32 text-center rounded-lg bg-gray-300 ">
-          <div className=" font-bold p-5 text-lg">Total Complexes</div>
-          <div className="p-5 ">{visible && data.totalComplex}</div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className=" font-bold p-5 text-3xl">Total Complexes</div>
+          <div className="p-5 text-2xl">{visible && data.totalComplex}</div>
         </div>
-        <div className="h-32 text-center rounded-lg bg-gray-300 ">
-          <div className=" font-bold p-5 text-lg">Total Sports</div>
-          <div className="p-5 ">{visible && data.totalSports}</div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className=" font-bold p-5 text-3xl">Total Sports</div>
+          <div className="p-5 text-2xl">{visible && data.totalSports}</div>
         </div>
-        <div className="h-32 text-center rounded-lg bg-gray-300 ">
-          <div className=" font-bold p-5 text-lg">Total Athlete</div>
-          <div className="p-5 ">{visible && data.totalathlete}</div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className=" font-bold p-5 text-3xl">Total Athlete</div>
+          <div className="p-5 text-2xl">{visible && data.totalathlete}</div>
         </div>
-        <div className="h-32 text-center rounded-lg bg-gray-300 ">
-          <div className=" font-bold p-5 text-lg">Total Complaints</div>
-          <div className="p-5 ">{visible && data.totalcomplaints}</div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className=" font-bold p-5 text-3xl">Total Complaints</div>
+          <div className="p-5 text-2xl">{visible && data.totalcomplaints}</div>
         </div>
-        <div className="h-32 text-center rounded-lg bg-gray-300 ">
-          <div className=" font-bold p-5 text-lg">Total Instructor</div>
-          <div className="p-5 ">{visible && data.totalinstructer}</div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className=" font-bold p-5 text-3xl">Total Instructor</div>
+          <div className="p-5 text-2xl">{visible && data.totalinstructer}</div>
         </div>
-        <div className="h-32 text-center rounded-lg bg-gray-300 ">
-          <div className=" font-bold p-5 text-lg">Total Manager</div>
-          <div className="p-5 ">{visible && data.totalmanager}</div>
+        <div className=" text-center rounded-lg bg-gray-300 ">
+          <div className=" font-bold p-5 text-3xl">Total Manager</div>
+          <div className="p-5 text-2xl">{visible && data.totalmanager}</div>
         </div>
-        <div className="h-32 text-center rounded-lg bg-gray-300 ">
-          <div className=" font-bold p-5 text-lg">Total Supervisor</div>
-          <div className="p-5 ">{visible && data.totalsupervisor}</div>
+        <div className="text-center rounded-lg bg-gray-300 ">
+          <div className=" font-bold p-5 text-3xl">Total Supervisor</div>
+          <div className="p-5 text-2xl">{visible && data.totalsupervisor}</div>
         </div>
       </div>
     </div>
