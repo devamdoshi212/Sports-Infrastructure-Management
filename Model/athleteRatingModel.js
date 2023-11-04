32a78a0859f71051dde1123970ae4ad81b7b21e4
AyushVChauhan <ayush2004chauhan2526@gmail.com>
2023-11-04 15:29:34 +0530

    athlete rating

---
+++ Model/athleteRatingModel.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require("mongoose");
+
+const athleteRatingSchema = new mongoose.Schema(
+    {
+        athleteId: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+        rating: Number,
+        sport: { type: mongoose.SchemaTypes.ObjectId, ref: "sports" },
+        sportComplex: {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: "sportscomplexes",
+        },
+        isEvaluated: { type: Number, default: 0 },
+        remarks: String,
+        supervisorId: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("athleteratings", athleteRatingSchema);
