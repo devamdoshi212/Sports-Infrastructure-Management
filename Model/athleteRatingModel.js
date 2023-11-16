4fd5f0d8ce76cde72d167673f0d5802d16177c41
Devam Doshi <devamdoshi212@gmail.com>
2023-11-16 19:38:14 +0530

    paramter try

--- Model/athleteRatingModel.js
+++ Model/athleteRatingModel.js
@@ -1,19 +1,25 @@
 const { default: mongoose } = require("mongoose");
 
 const athleteRatingSchema = new mongoose.Schema(
-    {
-        athleteId: { type: mongoose.SchemaTypes.ObjectId, ref: "athletes" },
-        rating: Number,
-        sport: { type: mongoose.SchemaTypes.ObjectId, ref: "sports" },
-        sportComplex: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "sportscomplexes",
-        },
-        isEvaluated: { type: Number, default: 0 },
-        remarks: String,
-        supervisorId: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+  {
+    athleteId: { type: mongoose.SchemaTypes.ObjectId, ref: "athletes" },
+    rating: Number,
+    sport: { type: mongoose.SchemaTypes.ObjectId, ref: "sports" },
+    sportComplex: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "sportscomplexes",
     },
-    { timestamps: true }
+    isEvaluated: { type: Number, default: 0 },
+    parameters: [
+      {
+        parameter: String,
+        value: Number,
+      },
+    ],
+    remarks: String,
+    supervisorId: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("athleteratings", athleteRatingSchema);
