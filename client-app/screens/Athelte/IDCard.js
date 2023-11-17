fef6ea21864b6a870a13d04c385fbbb8710d62a3
Devam Doshi <devamdoshi212@gmail.com>
2023-11-17 18:26:34 +0530

    UI chanegs

--- client-app/screens/Athelte/IDCard.js
+++ client-app/screens/Athelte/IDCard.js
@@ -1,11 +1,11 @@
 import {
-    View,
-    Text,
-    Image,
-    StyleSheet,
-    Button,
-    TouchableOpacity,
-    Pressable,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Pressable,
 } from "react-native";
 import ipconfig from "../../ipconfig";
 import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
@@ -14,37 +14,37 @@ import { useSelector } from "react-redux";
 import QRCodeGenerator from "./QRCodeUser";
 
 const IDCard = ({ navigation }) => {
-    const ip = ipconfig.ip;
-    const Userdata = useSelector((state) => state.user.User);
-    const Atheltedata = useSelector((state) => state.athelte.Athelte);
-    const [supervisorId, setsupervisorId] = useState("");
-    const [image, setimage] = useState("./../../assets/icon.png");
+  const ip = ipconfig.ip;
+  const Userdata = useSelector((state) => state.user.User);
+  const Atheltedata = useSelector((state) => state.athelte.Athelte);
+  const [supervisorId, setsupervisorId] = useState("");
+  const [image, setimage] = useState("./../../assets/icon.png");
 
-    useEffect(() => {
-        if (Atheltedata[0].createdBy) {
-            setsupervisorId(Atheltedata[0].createdBy._id);
-        }
-        const i = Atheltedata[0].baseUrl.slice(1);
-        setimage(i);
-    }, [image]);
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Pressable
-                    onPress={() => {
-                        navigation.goBack();
-                    }}
-                >
-                    <View style={styles.back}>
-                        <Ionicons name="arrow-back" size={24} />
-                    </View>
-                </Pressable>
-                <View style={styles.heading}>
-                    <Text style={{ fontWeight: "bold", fontSize: 25 }}>ID Card</Text>
-                </View>
-            </View>
-            <View style={styles.card}>
-                {/* <View style={styles.photo}>
+  useEffect(() => {
+    if (Atheltedata[0].createdBy) {
+      setsupervisorId(Atheltedata[0].createdBy._id);
+    }
+    const i = Atheltedata[0].baseUrl.slice(1);
+    setimage(i);
+  }, [image]);
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Pressable
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <View style={styles.back}>
+            <Ionicons name="arrow-back" size={24} />
+          </View>
+        </Pressable>
+        <View style={styles.heading}>
+          <Text style={{ fontWeight: "bold", fontSize: 25 }}>ID Card</Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        {/* <View style={styles.photo}>
                     <View
                         style={{ flexDirection: "row", padding: 10, alignItems: "center" }}
                     >
@@ -61,71 +61,74 @@ const IDCard = ({ navigation }) => {
                     </View>
                     <Text style={styles.input}>{Userdata.Name}</Text>
                 </View> */}
-                <View style={styles.profileDetail}>
-                    {supervisorId && <QRCodeGenerator value={supervisorId} size={300} />}
-                    <View>
-                        <Image
-                            style={{
-                                width: 70,
-                                height: 70,
-                                borderRadius: 62.5,
-                                marginTop: -185,
-                            }}
-                            source={{ uri: `http://${ip}:9999/${image}` }}
-                        />
-                    </View>
-                    <Text style={styles.input}>{Userdata.Email}</Text>
-                    <Text style={styles.input}>{Userdata.Name}</Text>
-                </View>
-            </View>
+        <View style={styles.profileDetail}>
+          {supervisorId && <QRCodeGenerator value={supervisorId} size={300} />}
+          <View>
+            <Image
+              style={{
+                width: 80,
+                height: 80,
+                borderRadius: 62.5,
+                marginTop: -185,
+                borderColor: "white",
+                borderWidth: 5,
+              }}
+              source={{ uri: `http://${ip}:9999/${image}` }}
+            />
+          </View>
+          <Text style={styles.input}>{Userdata.Email}</Text>
+          <Text style={styles.input}>{Userdata.Name}</Text>
         </View>
-    );
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#f0f0f0",
-        marginTop: 25,
-    },
-    header: {
-        margin: 20,
-        flexDirection: "row",
-        marginBottom: 10,
-        width: "100%",
-        height: 50,
-        backgroundColor: "#f0f0f0",
-        alignItems: "center",
-    },
-    back: {
-        marginHorizontal: 4,
-        alignSelf: "center",
-    },
-    heading: {
-        justifyContent: "center",
-        alignItems: "center",
-        width: "70%",
-    },
-    card: {
-        backgroundColor: "white",
-        padding: 20,
-        borderRadius: 10,
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 5,
-        marginHorizontal: 30,
-        marginVertical: 20,
-    },
-    profileDetail: {
-        alignItems: "center",
-    },
-    input: {
-        marginTop:"3.5%",
-        fontWeight: "bold",
-        fontSize:24
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fbe8e0",
+    marginTop: 25,
+  },
+  header: {
+    margin: 20,
+    flexDirection: "row",
+    marginBottom: 10,
+    width: "100%",
+    height: 50,
+    backgroundColor: "#fbe8e0",
+    alignItems: "center",
+  },
+  back: {
+    marginHorizontal: 4,
+    alignSelf: "center",
+  },
+  heading: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "70%",
+  },
+  card: {
+    backgroundColor: "#f2b69c",
+    padding: 20,
+    paddingTop: 50,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+    marginHorizontal: 30,
+    marginVertical: 100,
+  },
+  profileDetail: {
+    alignItems: "center",
+  },
+  input: {
+    marginTop: "3.5%",
+    fontWeight: "bold",
+    fontSize: 24,
+  },
 });
 
 export default IDCard;
--- client-app/screens/Athelte/Profile.js
+++ client-app/screens/Athelte/Profile.js
@@ -5,276 +5,300 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useDispatch, useSelector } from "react-redux";
 import { UserActions } from "../../store/User";
 import {
-    View,
-    Text,
-    Image,
-    StyleSheet,
-    Button,
-    TouchableOpacity,
-    Pressable,
-    ScrollView,
-    Dimensions,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Pressable,
+  ScrollView,
+  Dimensions,
+  Alert,
 } from "react-native";
 import ipconfig from "../../ipconfig";
 const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 async function removeItem() {
-    try {
-        await AsyncStorage.removeItem("token");
-    } catch (error) {
-        // Handle the error
-    }
+  try {
+    await AsyncStorage.removeItem("token");
+  } catch (error) {
+    // Handle the error
+  }
 }
 function Profile({ navigation }) {
-    const dispatch = useDispatch();
-    const ip = ipconfig.ip;
-    const Userdata = useSelector((state) => state.user.User);
-    const Atheltedata = useSelector((state) => state.athelte.Athelte);
-    const [image, setimage] = useState("./../../assets/icon.png");
+  const dispatch = useDispatch();
+  const ip = ipconfig.ip;
+  const Userdata = useSelector((state) => state.user.User);
+  const Atheltedata = useSelector((state) => state.athelte.Athelte);
+  const [image, setimage] = useState("./../../assets/icon.png");
 
-    const ProfileDetailHandler = () => {
-        console.log("hello");
-        navigation.navigate("DetailProfile");
-    };
+  const ProfileDetailHandler = () => {
+    // console.log("hello");
+    navigation.navigate("DetailProfile");
+  };
 
-    useEffect(() => {
-        // console.log(Atheltedata[0].baseUrl);
-        const i = Atheltedata[0].baseUrl.slice(1);
-        console.log(i);
-        setimage(i);
-    }, [image]);
-    // console.log(Userdata.base)
-    const handleLogout = () => {
-        dispatch(UserActions.getuserRole({ Role: "" }));
-        removeItem();
-        // Implement your logout logic here
-    };
-    return (
-        <ScrollView>
-            <View style={styles.container}>
-                <TouchableOpacity onPress={ProfileDetailHandler}>
-                    <View style={styles.profileCard}>
-                        <View style={styles.profileImage}>
-                            <Image
-                                style={{
-                                    width: 125,
-                                    height: 125,
-                                    borderRadius: 62.5,
-                                }}
-                                source={{ uri: `http://${ip}:9999/${image}` }}
-                            />
-                        </View>
-                        <View>
-                            <Text
-                                style={{
-                                    fontWeight: "bold",
-                                    fontSize: 20,
-                                    alignSelf: "center",
-                                    marginTop: "1%",
-                                }}
-                            >
-                                {Userdata.Name}
-                            </Text>
-                        </View>
-                    </View>
-                </TouchableOpacity>
-                {Atheltedata[0].createdBy && (
-                    <View style={styles.card}>
-                        <TouchableOpacity
-                            onPress={() => {
-                                navigation.navigate("LeaderBoard");
-                            }}
-                        >
-                            <View style={styles.row}>
-                                <Text style={styles.actionText}>Leaderboard</Text>
-                                <MaterialIcons name="navigate-next" size={24} />
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            onPress={() => {
-                                navigation.navigate("SportsComplexDetails");
-                            }}
-                        >
-                            <View style={styles.row}>
-                                <Text style={styles.actionText}>Sport Complex Details</Text>
-                                <MaterialIcons name="navigate-next" size={24} />
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            onPress={() => {
-                                navigation.navigate("IDCard");
-                            }}
-                        >
-                            <View style={styles.row}>
-                                <Text style={styles.actionText}>ID card</Text>
-                                <MaterialIcons name="navigate-next" size={24} />
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            onPress={() => {
-                                navigation.navigate("SportEnroll");
-                            }}
-                        >
-                            <View style={styles.row}>
-                                <Text style={styles.actionText}>Enrolled Sports</Text>
-                                <MaterialIcons name="navigate-next" size={24} />
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            onPress={() => {
-                                navigation.navigate("PaymentHistory");
-                            }}
-                        >
-                            <View style={styles.row}>
-                                <Text style={styles.actionText}>Payment History</Text>
-                                <MaterialIcons name="navigate-next" size={24} />
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            onPress={() => {
-                                navigation.navigate("ComplaintList");
-                            }}
-                        >
-                            <View style={styles.row}>
-                                <Text style={styles.actionText}>Complaint</Text>
-                                <MaterialIcons name="navigate-next" size={24} />
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            onPress={() => {
-                                navigation.navigate("ExitForm");
-                            }}
-                        >
-                            <View style={styles.row}>
-                                <Text style={styles.actionText}>Athelte Response</Text>
-                                <MaterialIcons name="navigate-next" size={24} />
-                            </View>
-                        </TouchableOpacity>
-                    </View>
-                )}
-                <TouchableOpacity style={styles.button} onPress={handleLogout}>
-                    <Text style={styles.buttonText}>Logout</Text>
-                </TouchableOpacity>
+  useEffect(() => {
+    // console.log(Atheltedata[0].baseUrl);
+    const i = Atheltedata[0].baseUrl.slice(1);
+    console.log(i);
+    setimage(i);
+  }, [image]);
+  // console.log(Userdata.base)
+  const handleLogout = () => {
+    dispatch(UserActions.getuserRole({ Role: "" }));
+    removeItem();
+    // Implement your logout logic here
+  };
+  return (
+    <ScrollView
+      style={{
+        backgroundColor: "#fbe8e0",
+      }}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity onPress={ProfileDetailHandler}>
+          <View style={styles.profileCard}>
+            <View style={styles.profileImage}>
+              <Image
+                style={{
+                  width: 125,
+                  height: 125,
+                  borderRadius: 62.5,
+                  borderColor: "#fbe8e0",
+                  borderWidth: 5,
+                }}
+                source={{ uri: `http://${ip}:9999/${image}` }}
+              />
             </View>
-        </ScrollView>
-    );
+            <View>
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 20,
+                  alignSelf: "center",
+                  marginTop: "1%",
+                }}
+              >
+                {Userdata.Name}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+        {Atheltedata[0].createdBy && (
+          <View style={styles.card}>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("LeaderBoard");
+              }}
+            >
+              <View style={styles.row}>
+                <Text style={styles.actionText}>Leaderboard</Text>
+                <MaterialIcons name="navigate-next" size={24} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("SportsComplexDetails");
+              }}
+            >
+              <View style={styles.row}>
+                <Text style={styles.actionText}>Sport Complex Details</Text>
+                <MaterialIcons name="navigate-next" size={24} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("IDCard");
+              }}
+            >
+              <View style={styles.row}>
+                <Text style={styles.actionText}>ID card</Text>
+                <MaterialIcons name="navigate-next" size={24} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("SportEnroll");
+              }}
+            >
+              <View style={styles.row}>
+                <Text style={styles.actionText}>Enrolled Sports</Text>
+                <MaterialIcons name="navigate-next" size={24} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("PaymentHistory");
+              }}
+            >
+              <View style={styles.row}>
+                <Text style={styles.actionText}>Payment History</Text>
+                <MaterialIcons name="navigate-next" size={24} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("ComplaintList");
+              }}
+            >
+              <View style={styles.row}>
+                <Text style={styles.actionText}>Complaint</Text>
+                <MaterialIcons name="navigate-next" size={24} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("ExitForm");
+              }}
+            >
+              <View style={styles.row}>
+                <Text style={styles.actionText}>Athelte Response</Text>
+                <MaterialIcons name="navigate-next" size={24} />
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            Alert.alert(
+              "Logout from the application",
+              "Are you sure you want to logout?",
+              [
+                {
+                  text: "CANCEL",
+                  onPress: () => console.log("Cancel"),
+                  style: "cancel",
+                },
+                { text: "LOGOUT", onPress: handleLogout },
+              ]
+            );
+          }}
+        >
+          <Text style={styles.buttonText}>Logout</Text>
+          <MaterialIcons name="logout" size={24} />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#f0f0f0",
-        marginTop: 25,
-    },
-    profileCard: {
-        borderRadius: 20,
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 5,
-        marginHorizontal: 15,
-        marginVertical: 5,
-        marginTop: "20%",
-        paddingBottom: "5%",
-        backgroundColor: "#f3f0f0",
-        flexDirection: "column",
-        borderWidth:1,
-    },
-    profileImage: {
-        flex: 1,
-        alignSelf: "center",
-        alignSelf: "center",
-        borderRadius: 15,
-        marginTop: "-15%",
-    },
-    card: {
-        flexDirection: "column",
-        marginTop: "1%",
-        alignItems: "center",
-        alignSelf: "center",
-        padding: 10,
-        width: "95%",
-        borderRadius: 10,
-        backgroundColor: "white",
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 5,
-        marginHorizontal: 15,
-        marginVertical: 5,
-        marginBottom: "2%",
-        paddingBottom: "5%",
-        backgroundColor: "#fbe8e0",
-    },
-    profileInfo: {
-        backgroundColor: "white",
-        padding: 20,
-        borderRadius: 10,
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 5,
-        width: "95%",
-        flexDirection: "row",
-        alignSelf: "center",
-        marginTop: "8%",
-        justifyContent: "space-between",
-    },
-    leftColumn: {
-        flex: 1,
-    },
-    rightColumn: {
-        flex: 2,
-        paddingLeft: 20,
-        paddingBottom: 8,
-        marginLeft: 6,
-    },
+  container: {
+    flex: 1,
+    // backgroundColor: "#fbe8e0",
+    marginTop: 30,
+  },
+  profileCard: {
+    borderRadius: 20,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+    marginHorizontal: 15,
+    marginVertical: 5,
+    marginTop: "20%",
+    paddingBottom: "5%",
+    backgroundColor: "#9cafa2",
+    flexDirection: "column",
+  },
+  profileImage: {
+    flex: 1,
+    alignSelf: "center",
+    alignSelf: "center",
+    borderRadius: 15,
+    borderColor: "white",
+    marginTop: "-15%",
+  },
+  card: {
+    flexDirection: "column",
+    marginTop: "1%",
+    alignItems: "center",
+    alignSelf: "center",
+    padding: 10,
+    width: "95%",
+    borderRadius: 10,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+    marginHorizontal: 15,
+    marginVertical: 5,
+    marginBottom: "2%",
+    paddingBottom: "5%",
+    backgroundColor: "#f2b69c",
+  },
+  profileInfo: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+    width: "100%",
+    flexDirection: "row",
+    alignSelf: "center",
+    marginTop: "8%",
+    justifyContent: "space-between",
+  },
+  leftColumn: {
+    flex: 1,
+  },
+  rightColumn: {
+    flex: 2,
+    paddingLeft: 20,
+    paddingBottom: 8,
+    marginLeft: 6,
+  },
 
-    name: {
-        fontSize: 16,
-        color: "grey",
-        marginTop: 5,
-    },
-    row: {
-        width: "120%",
-        marginTop: "5%",
-        flexDirection: "row",
-        borderBottomWidth: 1,
-        paddingVertical: "3%",
-    },
-    actionText: {
-        width: "75%",
-        fontWeight: "bold",
-        fontSize: 15,
-    },
-    logout: {
-        width: "40%",
-        alignSelf: "center",
-        marginTop: "15%",
-    },
-    button: {
-        flexDirection: "row",
-        marginTop: 10,
-        marginLeft: "3%",
-        width: "94%",
-        height: screenHeight * 0.06,
-        backgroundColor: "#000",
-        borderRadius: 3,
-        padding: 10,
-    },
-    buttonText: {
-        flex: 1,
-        textAlign: "center",
-        color: "#fff",
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    buttonImage: {
-        textAlign: "center",
-        color: "#fff",
-    },
+  name: {
+    fontSize: 16,
+    color: "grey",
+    marginTop: 5,
+  },
+  row: {
+    width: "120%",
+    marginTop: "5%",
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    paddingVertical: "3%",
+  },
+  actionText: {
+    width: "75%",
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  logout: {
+    width: "40%",
+    alignSelf: "center",
+    marginTop: "15%",
+  },
+  button: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginLeft: "3%",
+    width: "94%",
+    // height: screenHeight * 0.06,
+    backgroundColor: "#f2b69c",
+    borderRadius: 5,
+    padding: 10,
+  },
+  buttonText: {
+    flex: 1,
+    color: "black",
+    width: "75%",
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  buttonImage: {
+    textAlign: "center",
+    color: "#fff",
+  },
 });
 
 export default Profile;
