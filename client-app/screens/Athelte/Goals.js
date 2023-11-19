c82b2bddc32a8183a62bcfa74137750c45f0a500
Devam Doshi <devamdoshi212@gmail.com>
2023-11-19 12:09:50 +0530

    Goals screen UI Done

--- client-app/screens/Athelte/Goals.js
+++ client-app/screens/Athelte/Goals.js
@@ -6,8 +6,9 @@ import {
   ScrollView,
   Pressable,
 } from "react-native";
+import { Calendar } from "react-native-calendars";
+
 import { Ionicons, Entypo, AntDesign } from "@expo/vector-icons";
-import { Picker } from "@react-native-picker/picker";
 import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import ipconfig from "../../ipconfig";
@@ -15,30 +16,29 @@ import { useSelector } from "react-redux";
 
 const Goals = () => {
   const navigation = useNavigation();
-  const [selectedstatus, setselectedstatus] = useState("0");
-  const [Complaint, setcomplain] = useState([]);
+  const navigate = useNavigation();
+
   const ip = ipconfig.ip;
-  const Userdata = useSelector((state) => state.user.User);
   const [refresh, setrefresh] = useState(false);
+  const Userdata = useSelector((state) => state.user.User);
+
+  const [date, setDate] = useState(new Date());
+  const [dateCal, setDateCal] = useState(new Date());
+  const [achieved, setachived] = useState(0);
+  const [goals, setGoals] = useState([
+    {
+      title: "ABC",
+      description: "ABC 1",
+      targetdate: new Date().getDate(),
+      startdate: new Date(),
+    },
+  ]);
+
   const refreshhandler = () => {
     setrefresh(!refresh);
   };
-  useEffect(() => {
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-    fetch(
-      `http://${ip}:9999/getAllComplaints?userId=${Userdata._id}&status=${selectedstatus}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        setcomplain(result.data);
-      })
-      .catch((error) => console.log("error", error));
-  }, [selectedstatus, refresh]);
-  const navigate = useNavigation();
+
+  useEffect(() => {}, [refresh]);
   return (
     <>
       <View style={styles.container}>
@@ -55,6 +55,8 @@ const Goals = () => {
           <View style={styles.heading}>
             <Text style={{ fontWeight: "bold", fontSize: 25 }}>Goals</Text>
           </View>
+        </View>
+        <View style={styles.pickerContainer}>
           <TouchableOpacity
             onPress={() => {
               navigation.navigate("AddGoals");
@@ -74,30 +76,78 @@ const Goals = () => {
               />
             </View>
           </TouchableOpacity>
-        </View>
-        <View style={styles.pickerContainer}>
-          <Picker
-            selectedValue={selectedstatus}
-            onValueChange={(itemValue, itemIndex) => {
-              setselectedstatus(itemValue);
+          <Calendar
+            // Specify style for calendar container element. Default = {}
+            style={{
+              height: 350,
+              borderRadius: 15,
+              width: "95%",
+              alignSelf: "center",
             }}
-            style={styles.input}
-            placeholder="Select Complaint Status"
-          >
-            <Picker.Item label="Pending" value="0" />
-            <Picker.Item label="Solved" value="1" />
-          </Picker>
+            // Specify theme properties to override specific styles for calendar parts. Default = {}
+            theme={{
+              backgroundColor: "#ffffff",
+              calendarBackground: "#ffffff",
+              textSectionTitleColor: "#b6c1cd",
+              textSectionTitleDisabledColor: "#d9e1e8",
+              selectedDayBackgroundColor: "pink",
+              selectedDayTextColor: "#ffffff",
+              todayTextColor: "#00adf5",
+              dayTextColor: "#2d4150",
+              textDisabledColor: "#d9e1e8",
+              dotColor: "#00adf5",
+              selectedDotColor: "#ffffff",
+              arrowColor: "orange",
+              disabledArrowColor: "#d9e1e8",
+              monthTextColor: "#f2b69c",
+              indicatorColor: "#f2b69c",
+              textDayFontFamily: "monospace",
+              textMonthFontFamily: "monospace",
+              textDayHeaderFontFamily: "monospace",
+              textDayFontWeight: "300",
+              textMonthFontWeight: "bold",
+              textDayHeaderFontWeight: "300",
+              textDayFontSize: 16,
+              textMonthFontSize: 16,
+              textDayHeaderFontSize: 16,
+            }}
+            onDayPress={(day) => {
+              setDate(new Date(day.timestamp));
+              // console.log("selected date", day.dateString);
+              setDateCal(day.dateString);
+            }}
+            markedDates={{
+              [dateCal]: {
+                selected: true,
+                disableTouchEvent: true,
+                selectedDotColor: "orange",
+              },
+            }}
+            enableSwipeMonths={true}
+          />
+          <View style={styles.twobutton}>
+            <TouchableOpacity
+              style={achieved == 0 ? styles.button : styles.onpressbutton}
+              onPress={() => {
+                setachived(0);
+              }}
+            >
+              <Text style={styles.buttonText}>Pending</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={achieved == 1 ? styles.button : styles.onpressbutton}
+              onPress={() => {
+                setachived(1);
+              }}
+            >
+              <Text style={styles.buttonText}>Completed</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         <ScrollView contentContainerStyle={styles.scrollContainer}>
-          {Complaint.map((item, index) => (
+          {goals.map((item, index) => (
             <View style={styles.card} key={index}>
               <Pressable
-                onPress={() => {
-                  navigate.navigate("ComplaintView", {
-                    data: item,
-                    refreshhandler: refreshhandler,
-                  });
-                }}
                 style={({ pressed }) => [
                   {
                     backgroundColor: pressed ? "#f8d7c9" : "#f2b69c",
@@ -108,13 +158,11 @@ const Goals = () => {
               >
                 <View style={styles.row}>
                   <View style={styles.column1}>
-                    <Text style={styles.label}>Type</Text>
-                    <Text style={styles.label}>{item.type.Type}</Text>
-                  </View>
-                  <View style={styles.column2}>
-                    <Text style={styles.label}>
-                      {item.createdAt.split("T")[0]}
-                    </Text>
+                    <Text style={styles.titlelabel}>{item.title}</Text>
+                    <View style={styles.row}>
+                      <Text style={styles.label}>Target Date : </Text>
+                      <Text style={styles.label}>{item.targetdate}</Text>
+                    </View>
                   </View>
                   <View style={styles.column3}>
                     <Entypo style={{ color: "#0054a8" }} name="eye" size={24} />
@@ -151,15 +199,18 @@ const styles = StyleSheet.create({
   heading: {
     justifyContent: "flex-end",
     alignItems: "center",
-    width: "60%",
+    width: "95%",
   },
   complaint: {
     flexDirection: "row",
+    justifyContent: "center",
     width: "90%",
     // height: 40,
     borderRadius: 10,
     backgroundColor: "#f2b69c",
+    marginLeft: "5%",
     padding: "1%",
+    marginBottom: 10,
   },
   actionText: {
     marginLeft: "5%",
@@ -168,7 +219,7 @@ const styles = StyleSheet.create({
     color: "black",
   },
   pickerContainer: {
-    backgroundColor: "#f2b69c",
+    // backgroundColor: "#f2b69c",
     borderRadius: 5,
     marginBottom: 15,
     width: "90%",
@@ -215,9 +266,52 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 17,
   },
+  titlelabel: {
+    marginLeft: 10,
+    fontWeight: "bold",
+    fontSize: 17,
+  },
   input: {
     marginLeft: 6,
     textAlign: "center",
+  },
+  twobutton: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "90%",
+    // height: 40,
+    borderRadius: 10,
+    backgroundColor: "#fbe8e0",
+    marginLeft: "5%",
+    padding: "1%",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  buttonText1: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  onpressbutton: {
+    marginTop: 10,
+    width: "40%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 5,
+  },
+  button: {
+    marginTop: 10,
+    width: "40%",
+    backgroundColor: "#f2b69c",
+    borderRadius: 10,
+    padding: 5,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
