589acedc496ed29e90415d30f6e7742bb91ff9b6
HarshN187 <hnirmal913@gmail.com>
2023-11-21 16:28:16 +0530

    utilization controller change

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -6,7 +6,7 @@ const instructerModel = require("../Model/instructorModel");
 const complaintModel = require("../Model/ComplaintModel");
 const athleteModel = require("../Model/athleteModel");
 const userModel = require("../Model/UsersModel");
-const sessionModel = require("../Model/SessionModel");
+// const sessionModel = require("../Model/SessionModel");
 
 module.exports.AdminViewDetails = async function (req, res) {
   try {
@@ -135,90 +135,3 @@ module.exports.atheleteInSportsComplex = async function (req, res) {
   }
 };
 
-function countEntriesInTimeSlot(result, startHour, endHour) {
-  let count = 0;
-
-  result.forEach((ele) => {
-    const entryTime = ele.enrolls.entry;
-    // const timezoneOffset = entryTime.getTimezoneOffset();
-    console.log(entryTime);
-    const startTime = new Date(
-      entryTime.getFullYear(),
-      entryTime.getMonth(),
-      entryTime.getDate(),
-      startHour,
-      0,
-      0,
-      0
-    );
-    // Adjust for timezone offset
-    // startTime.setMinutes(startTime.getMinutes() - timezoneOffset);
-    console.log(startTime);
-    const endTime = new Date(
-      entryTime.getFullYear(),
-      entryTime.getMonth(),
-      entryTime.getDate(),
-      endHour,
-      0,
-      0,
-      0
-    );
-    // Adjust for timezone offset
-    // endTime.setMinutes(endTime.getMinutes() - timezoneOffset);
-    console.log(endTime);
-    if (entryTime >= startTime && entryTime < endTime) {
-      count++;
-    }
-  });
-
-  return count;
-}
-
-module.exports.utilization = async function (req, res) {
-  try {
-    const data = await sessionModel.aggregate([
-      {
-        $match: {
-          sportscomplex: new mongoose.Types.ObjectId(req.query.sportsComplexId),
-        },
-      },
-      {
-        $unwind: "$enrolls",
-      },
-    ]);
-    // let slot1 = countEntriesInTimeSlot(data, 7, 9);
-    // let slot2 = countEntriesInTimeSlot(data, 9, 11);
-    // let slot3 = countEntriesInTimeSlot(data, 11, 13);
-    // let slot4 = countEntriesInTimeSlot(data, 13, 15);
-    // let slot5 = countEntriesInTimeSlot(data, 15, 17);
-    // let slot6 = countEntriesInTimeSlot(data, 17, 19);
-    // let slot7 = countEntriesInTimeSlot(data, 19, 24);
-    // let slot8 = countEntriesInTimeSlot(data, 0, 7);
-    let slotCounts = [];
-
-    slotCounts.push(countEntriesInTimeSlot(data, 7, 9));
-    slotCounts.push(countEntriesInTimeSlot(data, 9, 11));
-    slotCounts.push(countEntriesInTimeSlot(data, 11, 13));
-    slotCounts.push(countEntriesInTimeSlot(data, 13, 15));
-    slotCounts.push(countEntriesInTimeSlot(data, 15, 17));
-    slotCounts.push(countEntriesInTimeSlot(data, 17, 19));
-    slotCounts.push(countEntriesInTimeSlot(data, 19, 24));
-    slotCounts.push(countEntriesInTimeSlot(data, 0, 7));
-
-    res.json({
-      total: data.length,
-      slotcount: slotCounts,
-      // slot1: slot1,
-      // slot2: slot2,
-      // slot3: slot3,
-      // slot4: slot4,
-      // slot5: slot5,
-      // slot6: slot6,
-      // slot7: slot7,
-      // slot8: slot8,
-      // data: data,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
