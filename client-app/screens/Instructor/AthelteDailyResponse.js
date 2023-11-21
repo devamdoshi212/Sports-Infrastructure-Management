caa4c75fb1f23b650c08fd6147f8a53f370bcbc1
Rits0631 <94890637+Ritesh0604@users.noreply.github.com>
2023-11-21 22:31:11 +0530

    instructor css

--- client-app/screens/Instructor/AthelteDailyResponse.js
+++ client-app/screens/Instructor/AthelteDailyResponse.js
@@ -1,12 +1,12 @@
 import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  Button,
-  TouchableOpacity,
-  ScrollView,
-  Pressable,
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    Button,
+    TouchableOpacity,
+    ScrollView,
+    Pressable,
 } from "react-native";
 import RatingModal from "./RatingModal";
 import { Ionicons } from "@expo/vector-icons";
@@ -15,185 +15,181 @@ import { useNavigation } from "@react-navigation/native";
 import ipconfig from "../../ipconfig";
 import { useSelector } from "react-redux";
 const AthelteDailyResponse = () => {
-  const Userdata = useSelector((state) => state.user.User);
-  // console.log(Atheltedata[0].payments[0].sports);
-  //   const id = Atheltedata[0]._id;
-  const [rateModal, setRateModal] = useState(false);
-  const [rateId, setRateId] = useState("");
-  const [rating, setrating] = useState("");
-  const ip = ipconfig.ip;
-  const [data, setData] = useState([]);
-  const [refresh, setrefersh] = useState(false);
-  const refreshHandler = () => {
-    setrefersh(!refresh);
-  };
-  useEffect(() => {
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
+    const Userdata = useSelector((state) => state.user.User);
+    // console.log(Atheltedata[0].payments[0].sports);
+    //   const id = Atheltedata[0]._id;
+    const [rateModal, setRateModal] = useState(false);
+    const [rateId, setRateId] = useState("");
+    const [rating, setrating] = useState("");
+    const ip = ipconfig.ip;
+    const [data, setData] = useState([]);
+    const [refresh, setrefersh] = useState(false);
+    const refreshHandler = () => {
+        setrefersh(!refresh);
     };
-    // console.log(Userdata._id);
-    fetch(
-      `http://${ip}:9999/ratingForInstructor?instructorId=${Userdata._id}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        setData(result.ratings);
-      })
-      .catch((error) => console.log("error", error));
-  }, [refresh]);
+    useEffect(() => {
+        var requestOptions = {
+            method: "GET",
+            redirect: "follow",
+        };
+        // console.log(Userdata._id);
+        fetch(
+            `http://${ip}:9999/ratingForInstructor?instructorId=${Userdata._id}`,
+            requestOptions
+        )
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result);
+                setData(result.ratings);
+            })
+            .catch((error) => console.log("error", error));
+    }, [refresh]);
 
-  const ratingcountHandler = (id) => {
-    setrating(id);
-  };
-  const ratingHandler = (id) => {
-    console.log(id);
-    setRateModal(!rateModal);
-    setRateId(id);
-  };
-  const navigate = useNavigation();
-  return (
-    <>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Pressable
-            onPress={() => {
-              navigate.goBack();
-            }}
-          >
-            {rateModal && (
-              <RatingModal
-                rateID={rateId}
-                setrating={ratingcountHandler}
-                rateModal={rateModal}
-                refreshHandler={refreshHandler}
-              ></RatingModal>
-            )}
-            <View style={styles.back}>
-              <Ionicons name="arrow-back" size={24} />
+    const ratingcountHandler = (id) => {
+        setrating(id);
+    };
+    const ratingHandler = (id) => {
+        console.log(id);
+        setRateModal(!rateModal);
+        setRateId(id);
+    };
+    const navigate = useNavigation();
+    return (
+        <>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Pressable
+                        onPress={() => {
+                            navigate.goBack();
+                        }}
+                    >
+                        {rateModal && (
+                            <RatingModal
+                                rateID={rateId}
+                                setrating={ratingcountHandler}
+                                rateModal={rateModal}
+                                refreshHandler={refreshHandler}
+                            ></RatingModal>
+                        )}
+                        <View style={styles.back}>
+                            <Ionicons name="arrow-back" size={24} />
+                        </View>
+                    </Pressable>
+                    <View style={styles.heading}>
+                        <Text style={{ fontWeight: "bold", fontSize: 25 }}>
+                            Athelte Daily Response
+                        </Text>
+                    </View>
+                </View>
+                <ScrollView contentContainerStyle={styles.scrollContainer}>
+                    {data.map((item, index) => (
+                        <View style={styles.card} key={index}>
+                            <Pressable
+                                onPress={() => {
+                                    // setRateId(item._id);
+                                }}
+                                style={({ pressed }) => [
+                                    {
+                                        backgroundColor: pressed ? "#f0f0f0" : "white",
+                                        padding: 20,
+                                        borderRadius: 10,
+                                    },
+                                ]}
+                            >
+                                <View style={styles.row}>
+                                    <Text style={styles.label}>Name : </Text>
+                                    <Text style={styles.input}>{item.athleteId.userId.Name}</Text>
+                                </View>
+                                <View style={styles.row}>
+                                    <Text style={styles.label}>Remarks : </Text>
+                                    <Text style={styles.input}>{item.remarks}</Text>
+                                </View>
+                                <View style={styles.row}>
+                                    <Text style={styles.label}>Sport : </Text>
+                                    <Text style={styles.input}>{item.sport.SportName}</Text>
+                                </View>
+                                <View style={styles.row}>
+                                    <Text style={styles.label}>Parameter : </Text>
+                                </View>
+
+                                {item.parameters.map((item, index) => (
+                                    <View style={styles.row} key={index}>
+                                        <Text style={styles.label}>{item.parameter}</Text>
+                                        <Text style={styles.input}>{item.value}</Text>
+                                    </View>
+                                ))}
+
+                                <View style={styles.row}>
+                                    <Text style={styles.label}>Date : </Text>
+                                    <Text style={styles.input}>
+                                        {item.createdAt.split("T")[0]}
+                                    </Text>
+                                </View>
+                                <Pressable onPress={ratingHandler.bind(this, item._id)}>
+                                    <View style={{ alignSelf: "flex-end", padding: 10 }}>
+                                        <Text style={{ color: "#0054a8" }}>Rate us</Text>
+                                    </View>
+                                </Pressable>
+                            </Pressable>
+                        </View>
+                    ))}
+                </ScrollView>
             </View>
-          </Pressable>
-          <View style={styles.heading}>
-            <Text style={{ fontWeight: "bold", fontSize: 25 }}>
-              Athelte Daily Response
-            </Text>
-          </View>
-        </View>
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          {data.map((item, index) => (
-            <View style={styles.card} key={index}>
-              <Pressable
-                onPress={() => {
-                  // setRateId(item._id);
-                }}
-                style={({ pressed }) => [
-                  {
-                    backgroundColor: pressed ? "#f0f0f0" : "white",
-                    padding: 20,
-                    borderRadius: 10,
-                  },
-                ]}
-              >
-                <View style={styles.row}>
-                  <Text style={styles.label}>Name : </Text>
-                  <Text style={styles.input}>{item.athleteId.userId.Name}</Text>
-                </View>
-                <View style={styles.row}>
-                  <Text style={styles.label}>Remarks : </Text>
-                  <Text style={styles.input}>{item.remarks}</Text>
-                </View>
-                <View style={styles.row}>
-                  <Text style={styles.label}>Sport : </Text>
-                  <Text style={styles.input}>{item.sport.SportName}</Text>
-                </View>
-                <View style={styles.row}>
-                  <Text style={styles.label}>Parameter : </Text>
-                </View>
-
-                {item.parameters.map((item, index) => (
-                  <View style={styles.row} key={index}>
-                    <Text style={styles.label}>{item.parameter}</Text>
-                    <Text style={styles.input}>{item.value}</Text>
-                  </View>
-                ))}
-
-                <View style={styles.row}>
-                  <Text style={styles.label}>Date : </Text>
-                  <Text style={styles.input}>
-                    {item.createdAt.split("T")[0]}
-                  </Text>
-                </View>
-                <Pressable onPress={ratingHandler.bind(this, item._id)}>
-                  <View style={{ alignSelf: "flex-end", padding: 10 }}>
-                    <Text style={{ color: "#0054a8" }}>Rate us</Text>
-                  </View>
-                </Pressable>
-              </Pressable>
-            </View>
-          ))}
-        </ScrollView>
-      </View>
-    </>
-  );
+        </>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    paddingVertical: 50,
-    backgroundColor: "#f0f0f0",
-  },
-  header: {
-    flexDirection: "row",
-    marginBottom: 10,
-    width: "100%",
-    height: 50,
-    backgroundColor: "#f0f0f0",
-    alignItems: "center",
-  },
-  back: {
-    marginHorizontal: 4,
-    alignSelf: "center",
-  },
-  heading: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: "90%",
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    padding: 10,
-  },
-  card: {
-    backgroundColor: "white",
-    borderRadius: 10,
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
-    marginHorizontal: 15,
-    marginVertical: 10,
-  },
-  inline: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-  },
-  label: {
-    fontWeight: "bold",
-    fontSize: 17,
-  },
-  input: {
-    marginLeft: 6,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fbe8e0",
+        paddingTop: "7%",
+    },
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 10,
+        width: "90%",
+        paddingLeft: "5%",
+        height: 50,
+        backgroundColor: "#fbe8e0",
+    },
+    back: {
+        marginHorizontal: 4,
+        alignSelf: "center",
+    },
+    heading: {
+        justifyContent: "center",
+        alignItems: "center",
+        width: "90%",
+    },
+    scrollContainer: {
+        flexGrow: 1,
+        padding: 10,
+    },
+    card: {
+        borderRadius: 10,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 5,
+        marginHorizontal: 15,
+        marginVertical: 10,
+        backgroundColor: "#f2b69c",
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    label: {
+        fontWeight: "bold",
+        fontSize: 17,
+    },
+    input: {
+        marginLeft: 6,
+    },
 });
 
 export default AthelteDailyResponse;
--- client-app/screens/Instructor/Profile.js
+++ client-app/screens/Instructor/Profile.js
@@ -3,219 +3,217 @@ import { useDispatch, useSelector } from "react-redux";
 import { UserActions } from "../../store/User";
 import React, { useEffect, useState } from "react";
 import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  Button,
-  TouchableOpacity,
-  Pressable,
-  ScrollView,
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    Button,
+    TouchableOpacity,
+    Pressable,
+    ScrollView,
+    Alert
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 import ipconfig from "../../ipconfig";
 async function removeItem() {
-  try {
-    await AsyncStorage.removeItem("token");
-  } catch (error) {
-    // Handle the error
-  }
+    try {
+        await AsyncStorage.removeItem("token");
+    } catch (error) {
+        // Handle the error
+    }
 }
 function Profile({ navigation }) {
-  const dispatch = useDispatch();
-  const ip = ipconfig.ip;
-  const Userdata = useSelector((state) => state.user.User);
-  const [image, setimage] = useState("./../../assets/Instructor.jpg");
+    const dispatch = useDispatch();
+    const ip = ipconfig.ip;
+    const Userdata = useSelector((state) => state.user.User);
+    const [image, setimage] = useState("./../../assets/Instructor.jpg");
 
-  useEffect(() => {}, [image]);
-  const handleLogout = () => {
-    dispatch(UserActions.getuserRole({ Role: "" }));
-    removeItem();
-  };
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          {/* <View style={styles.back}>
-          <Ionicons name="arrow-back" size={24} />
-        </View> */}
-          <View style={styles.heading}>
-            <Text style={{ fontWeight: "bold", fontSize: 25 }}>My Profile</Text>
-          </View>
-        </View>
-        <Pressable
-          onPress={() => {
-            navigation.navigate("DetailProfile");
-          }}
-        >
-          <View style={styles.SupervisorInfo}>
-            <View style={styles.leftColumn}>
-              <Image
-                style={{
-                  width: 100,
-                  height: 100,
-                  borderRadius: 50,
-                  marginLeft: 1,
-                }}
-                source={require("./../../assets/Instructor.jpg")}
-              />
-            </View>
-            <View style={styles.rightColumn}>
-              <Text style={styles.name}>Name : {Userdata.Name}</Text>
-              <Text style={styles.name}>Email : {Userdata.Email}</Text>
-              <Text style={styles.name}>ContactNo : {Userdata.ContactNum}</Text>
-            </View>
-          </View>
-        </Pressable>
+    useEffect(() => { }, [image]);
+    const handleLogout = () => {
+        dispatch(UserActions.getuserRole({ Role: "" }));
+        removeItem();
+    };
+    return (
+        <ScrollView style={{
+            backgroundColor: "#fbe8e0",
+        }}>
+            <View style={styles.container}>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate("DetailProfile");
+                    }}
+                >
+                    <View style={styles.profileCard}>
+                        <View style={styles.profileImage}>
+                            <Image
+                                style={{
+                                    width: 125,
+                                    height: 125,
+                                    borderRadius: 62.5,
+                                    borderColor: "#fbe8e0",
+                                    borderWidth: 5,
+                                    backgroundColor: "white",
+                                }}
+                                source={require("../../assets/Supervisor.png")}
+                            />
+                        </View>
+                        <View>
+                            <Text
+                                style={{
+                                    fontWeight: "bold",
+                                    fontSize: 20,
+                                    alignSelf: "center",
+                                    marginTop: "1%",
+                                }}
+                            >
+                                {Userdata.Name}
+                            </Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
 
-        <View style={styles.actions}>
-          {/* <TouchableOpacity style={styles.actionButton}>
-          <View style={styles.row}>
-            <Text style={styles.actionText}>Sport Complex Details</Text>
-          </View>
-        </TouchableOpacity> */}
-          <TouchableOpacity
-            style={styles.actionButton}
-            onPress={() => {
-              navigation.navigate("AthelteDetails");
-            }}
-          >
-            <View style={styles.row}>
-              <Text style={styles.actionText}>Athelte Details</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.actionButton}
-            onPress={() => {
-              //   navigation.navigate("AthelteDetails");
-            }}
-          >
-            <View style={styles.row}>
-              <Text style={styles.actionText}>Custom Notifications</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.actionButton}
-            onPress={() => {
-              navigation.navigate("ComplexDetails");
-            }}
-          >
-            <View style={styles.row}>
-              <Text style={styles.actionText}>Complex Details</Text>
-            </View>
-          </TouchableOpacity>
+                <View style={styles.card}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("AthelteDetails");
+                        }}
+                    >
+                        <View style={styles.row}>
+                            <Text style={styles.actionText}>Athelte Details</Text>
+                            <MaterialIcons name="navigate-next" size={24} />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            //   navigation.navigate("AthelteDetails");
+                        }}
+                    >
+                        <View style={styles.row}>
+                            <Text style={styles.actionText}>Custom Notifications</Text>
+                            <MaterialIcons name="navigate-next" size={24} />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("ComplexDetails");
+                        }}
+                    >
+                        <View style={styles.row}>
+                            <Text style={styles.actionText}>Complex Details</Text>
+                            <MaterialIcons name="navigate-next" size={24} />
+                        </View>
+                    </TouchableOpacity>
 
-          <TouchableOpacity
-            style={styles.actionButton}
-            onPress={() => {
-              navigation.navigate("AthelteDailyResponse");
-            }}
-          >
-            <View style={styles.row}>
-              <Text style={styles.actionText}>Athelte Daily Response</Text>
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("AthelteDailyResponse");
+                        }}
+                    >
+                        <View style={styles.row}>
+                            <Text style={styles.actionText}>Athelte Daily Response</Text>
+                            <MaterialIcons name="navigate-next" size={24} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        Alert.alert(
+                            "Logout from the application",
+                            "Are you sure you want to logout?",
+                            [
+                                {
+                                    text: "CANCEL",
+                                    onPress: () => console.log("Cancel"),
+                                    style: "cancel",
+                                },
+                                { text: "LOGOUT", onPress: handleLogout },
+                            ]
+                        );
+                    }}
+                >
+                    <Text style={styles.buttonText}>Logout</Text>
+                    <MaterialIcons name="logout" size={24} />
+                </TouchableOpacity>
             </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.actionButton}>
-            <View
-              style={{ width: "40%", alignSelf: "center", marginTop: "3%" }}
-            >
-              <Button color="#FF3D3D" title="Logout" onPress={handleLogout} />
-              {/* <MaterialCommunityIcons name="logout" size={24} /> */}
-              {/* <Button title="Logout" style={styles.actionText}/> */}
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </ScrollView>
-  );
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f0f0f0",
-    marginTop: 25,
-  },
-  header: {
-    margin: 20,
-    flexDirection: "row",
-    marginBottom: 10,
-    width: "100%",
-    height: 50,
-    backgroundColor: "#f0f0f0",
-    alignItems: "center",
-  },
-  heading: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: "70%",
-  },
-  SupervisorInfo: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
-    width: "95%",
-    flexDirection: "row",
-    alignSelf: "center",
-    marginTop: "5%",
-    justifyContent: "space-between",
-  },
-  leftColumn: {
-    flex: 1,
-  },
-  rightColumn: {
-    flex: 2,
-    paddingLeft: 20,
-    paddingBottom: 8,
-    marginLeft: 6,
-  },
-  SupervisorImage: {
-    width: 80,
-    height: 80,
-    borderRadius: 60,
-  },
-  name: {
-    fontSize: 16,
-    color: "grey",
-    marginTop: 5,
-  },
-  row: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
-    width: "100%",
-    flexDirection: "row",
-  },
-  actions: {
-    marginTop: 5,
-    width: "95%",
-    alignSelf: "center",
-  },
-  actionButton: {
-    paddingVertical: 10,
-  },
-  scrollContainer: {
-    // flexGrow: 1,
-    width: "90%",
-    padding: 10,
-    borderWidth: 1,
-  },
-  actionText: {
-    width: "80%",
-
-    fontWeight: "bold",
-    fontSize: 15,
-  },
+    container: {
+        flex: 1,
+    },
+    profileCard: {
+        borderRadius: 20,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 5,
+        marginHorizontal: 15,
+        marginVertical: 5,
+        marginTop: "20%",
+        paddingBottom: "5%",
+        backgroundColor: "#9cafa2",
+        flexDirection: "column",
+    },
+    profileImage: {
+        flex: 1,
+        alignSelf: "center",
+        alignSelf: "center",
+        borderRadius: 15,
+        borderColor: "white",
+        marginTop: "-15%",
+    },
+    card: {
+        flexDirection: "column",
+        marginTop: "1%",
+        alignItems: "center",
+        alignSelf: "center",
+        padding: 10,
+        width: "95%",
+        borderRadius: 10,
+        backgroundColor: "white",
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 5,
+        marginHorizontal: 15,
+        marginVertical: 5,
+        marginBottom: "2%",
+        paddingBottom: "5%",
+        backgroundColor: "#f2b69c",
+    },
+    row: {
+        width: "120%",
+        marginTop: "5%",
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        paddingVertical: "3%",
+    },
+    actionText: {
+        width: "75%",
+        fontWeight: "bold",
+        fontSize: 15,
+    },
+    button: {
+        flexDirection: "row",
+        marginTop: 10,
+        marginLeft: "3%",
+        width: "94%",
+        backgroundColor: "#f2b69c",
+        borderRadius: 5,
+        padding: 10,
+    },
+    buttonText: {
+        flex: 1,
+        color: "black",
+        width: "75%",
+        fontWeight: "bold",
+        fontSize: 15,
+    },
 });
 
 export default Profile;
